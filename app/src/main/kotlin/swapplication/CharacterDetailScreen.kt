8d9c30acb10151bee43c794adc81b8a29a3ffcda
akopylov95 <akopylov95@mail.ru>
2025-02-26 18:36:17 +0300

    Add TopAppBar

--- app/src/main/kotlin/swapplication/CharacterDetailScreen.kt
+++ app/src/main/kotlin/swapplication/CharacterDetailScreen.kt
@@ -1,6 +1,12 @@
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -8,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -15,6 +22,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import swapplication.CharacterDetailViewModel
 import swapplication.StarWarsCharacter
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CharacterDetailScreen(
     characterId: String,
@@ -25,11 +33,12 @@ fun CharacterDetailScreen(
     }
 
     val state by viewModel.state.collectAsStateWithLifecycle()
-
+    val backDispatcher = LocalOnBackPressedDispatcherOwner.current?.onBackPressedDispatcher
     if (state.isLoading) {
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .background(Color.Black)
         ){
             Box(
                 modifier = Modifier
@@ -42,60 +51,131 @@ fun CharacterDetailScreen(
         Text(text = "Ошибка: ${state.error}")
     } else {
         state.character?.let { character ->
-            CharacterInfo(character, characterId)
+//            TopAppBar(title= { Text("People", fontSize = 22.sp, color = Color.White)},
+//                navigationIcon = {
+//                    IconButton(onClick = { backDispatcher?.onBackPressed() }) {
+//                        Icon(
+//                            imageVector = Icons.Filled.ArrowBack,
+//                            tint = Color.White,
+//                            contentDescription = "Localized description"
+//
+//                        )
+//                    }
+//                },)
+
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        title = {
+//                            Box(modifier = Modifier
+//                                .fillMaxWidth()
+//                                .background(Color.Black),
+//                                contentAlignment = Alignment.Center)
+//                            {
+//                                Text(
+//                                    text = "Character",
+//                                    color = Color.White,
+//                                    fontSize = 24.sp,
+//                                    fontWeight = FontWeight.Bold,
+//                                    modifier = Modifier
+//                                        .padding(bottom = 16.dp)
+//
+//                                )
+//                            }
+                            Text(
+                                text = "Character",
+                                color = Color.White,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .padding(16.dp)
+
+                            )
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = { backDispatcher?.onBackPressed() }) {
+                                Icon(
+                                    imageVector = Icons.Filled.ArrowBack,
+                                    tint = Color.White,
+                                    contentDescription = "Localized description"
+
+                                )
+                            }
+                        },
+                        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                            containerColor = Color.Black,
+                            titleContentColor = Color.White
+                        ),
+                    )
+                }
+            ) { innerPadding ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CharacterInfo(character, characterId)
+                }
+            }
+
+            //CharacterInfo(character, characterId)
         }
     }
 }
 
+
 @Composable
 fun CharacterInfo(character: StarWarsCharacter, characterId: String) {
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.Black)
             .padding(16.dp),
-    )
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        shape = RoundedCornerShape(12.dp),
-        colors = CardDefaults.cardColors(containerColor = Color(0x33767676)),
-        elevation = CardDefaults.cardElevation(8.dp)
-    ) {
-        Column(
-            modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = RoundedCornerShape(12.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0x33767676)),
+            elevation = CardDefaults.cardElevation(8.dp)
         ) {
-            Text(
-                text = character.name,
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
-
-            Spacer(modifier = Modifier.height(8.dp))
-
-            AssetImage(
-                "$characterId.jpg",
-                modifier = Modifier
-                    .height(520.dp)
-                    .width(360.dp)
-                    //.size(360.dp)
-                    .clip(RoundedCornerShape(8.dp)),
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = character.name,
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White
                 )
 
-            Spacer(modifier = Modifier.height(16.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
-            Text(text = "Appearance", color = Color.Gray, fontSize = 16.sp)
+                AssetImage(
+                    "$characterId.jpg",
+                    modifier = Modifier
+                        .height(520.dp)
+                        .width(360.dp)
+                        //.size(360.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                )
 
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(4.dp)
-            ) {
-                InfoRow("Height", character.height)
-                InfoRow("Mass", character.mass)
-                InfoRow("Gender", character.gender)
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Text(text = "Appearance", color = Color.Gray, fontSize = 16.sp)
+
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    InfoRow("Height", character.height)
+                    InfoRow("Mass", character.mass)
+                    InfoRow("Gender", character.gender)
+                }
             }
         }
     }
--- app/src/main/kotlin/swapplication/PeopleListScreen.kt
+++ app/src/main/kotlin/swapplication/PeopleListScreen.kt
@@ -13,11 +13,13 @@ import swapplication.PeopleListViewModel
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -25,6 +27,7 @@ import com.example.swapp.R
 import swapplication.StarWarsCharacter
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PeopleListScreen(
     viewModel: PeopleListViewModel = viewModel(),
@@ -37,6 +40,7 @@ fun PeopleListScreen(
         Box(
             modifier = Modifier
                 .fillMaxSize()
+                .background(Color.Black)
         ){
             Box(
                 modifier = Modifier
@@ -48,32 +52,71 @@ fun PeopleListScreen(
     } else if (state.error != null) {
         Text(text = "Ошибка: ${state.error}")
     } else {
+//            Text(
+//                text = "People",
+//                color = Color.White,
+//                fontSize = 24.sp,
+//                fontWeight = FontWeight.Bold,
+//                modifier = Modifier.padding(bottom = 16.dp)
+//            )
 
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Black)
-                .padding(16.dp),
-            //shape = RoundedCornerShape(size = 9.6.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "People",
-                color = Color.White,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+        val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+//                        Box(modifier = Modifier
+//                            .fillMaxWidth()
+//                            .background(Color.Black),
+//                            contentAlignment = Alignment.Center) {
+//                            Text(
+//                                text = "People",
+//                                color = Color.White,
+//                                fontSize = 24.sp,
+//                                fontWeight = FontWeight.Bold,
+//                                modifier = Modifier
+//                                    .padding(bottom = 16.dp)
+//
+//                            )
+//                        }
+                        Text(
+                                text = "People",
+                                color = Color.White,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold,
+                                modifier = Modifier
+                                    .padding(16.dp)
 
-            LazyColumn (
-                verticalArrangement = Arrangement.spacedBy(12.dp),
-            ){
-                items(state.people) { character ->
-                    val characterId = character.url.split("/").filter { it.isNotEmpty() }.last() //  Получаем ID из URL
-                    PeopleInfo(character, characterId, onCharacterClick)
+                            )
+                    },
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = Color.Black,
+                        titleContentColor = Color.White
+                    ),
+                    scrollBehavior = scrollBehavior
+                )
+            }
+        ){innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Black)
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                LazyColumn (
+                    contentPadding = innerPadding,
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                ){
+                    items(state.people) { character ->
+                        val characterId = character.url.split("/").filter { it.isNotEmpty() }.last() //  Получаем ID из URL
+                        PeopleInfo(character, characterId, onCharacterClick)
+                    }
                 }
             }
         }
+
     }
 }
 
